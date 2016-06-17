c696522eba9c2a13c5a32e1b93a3269ad3e7cc98
zheng_b <arnaud.zg@gmail.com>
2016-06-18 02:03:16 +0800

    Add Android Studio project with OpenCV

---
+++ app/src/main/java/com/arnaudzheng/vehicletrackr/MainActivity.java
@@ -0,0 +1,25 @@
+package com.arnaudzheng.vehicletrackr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.OpenCVLoader;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    static {
+        if(!OpenCVLoader.initDebug()){
+            Log.d(TAG, "OpenCV not loaded");
+        } else {
+            Log.d(TAG, "OpenCV loaded");
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
